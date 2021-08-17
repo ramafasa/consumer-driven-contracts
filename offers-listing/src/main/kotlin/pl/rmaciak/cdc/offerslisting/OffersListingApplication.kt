5b589b5d5154126086fcbfb69e74f11d8ddc6f45
ramafasa <rafalmaciak@gmail.com>
2021-08-17 16:17:23 +0200

    Added offers-listing. Renamed offers -> offers-db

---
+++ offers-listing/src/main/kotlin/pl/rmaciak/cdc/offerslisting/OffersListingApplication.kt
@@ -0,0 +1,11 @@
+package pl.rmaciak.cdc.offerslisting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OffersListingApplication
+
+fun main(args: Array<String>) {
+	runApplication<OffersListingApplication>(*args)
+}
